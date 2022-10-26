9488ea1e686be99fb2a113f4270ce6b50a4a1809
Daniel Sousa <jdaniel.asousa@gmail.com>
2022-10-26 08:06:07 +0100

    feat(wip): starship section

---
+++ data/constants/urls.ts
@@ -0,0 +1,8 @@
+export const URLs = {
+	fish: 'https://fishshell.com/)',
+	ohMyFish: 'https://github.com/oh-my-fish/oh-my-fish',
+	zsh: '',
+	ohMyZsh: '',
+	starship: 'https://starship.rs/',
+	nerdFont: 'https://www.nerdfonts.com/'
+};
