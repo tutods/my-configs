51b04977b261cd9f353ad8c01876eeb2f60992f4
Daniel Sousa <jdaniel.asousa@gmail.com>
2022-10-26 20:52:47 +0100

    feat: update styles

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -2,13 +2,13 @@ import { AnchorHTMLAttributes, ReactNode } from 'react';
 
 type Props = AnchorHTMLAttributes<HTMLAnchorElement> & {
 	className?: string;
-	children: ReactNode;
+	content: ReactNode;
 };
 
-export const ExternalLink = ({ className = '', href, children, ...props }: Props) => {
+export const ExternalLink = ({ className = '', href, content, ...props }: Props) => {
 	return (
 		<a {...props} className={`${className}`} href={href} rel={'noreferrer'} target={'_blank'}>
-			{children}
+			{content}
 		</a>
 	);
 };
