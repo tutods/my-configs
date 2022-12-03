6fdc511ff2bfaf7d3b7b5a3bfd0aeacdf0e2c8f7
Daniel Sousa @TutoDS <jdaniel.asousa@gmail.com>
2022-12-03 14:16:26 +0000

    feat: update config

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import { DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
-	logo: <span>My Project</span>,
+	logo: (
+		<>
+			<span className={'mr-2 hidden font-extrabold md:inline'}>TutoDS</span>
+			<span className={'hidden font-normal text-gray-600 md:inline'}>
+				My Configurations for Fish, ZSH and more..
+			</span>
+		</>
+	),
 	project: {
 		link: 'https://github.com/tutods/my-configs'
 	},
 	docsRepositoryBase: 'https://github.com/tutods/my-configs/blob/master',
 	editLink: {
-		component: null
+		component: undefined
 	},
 	footer: {
 		text: (
@@ -26,12 +33,27 @@ const config: DocsThemeConfig = {
 			</>
 		)
 	},
+	head: (
+		<>
+			<meta content={'width=device-width, initial-scale=1.0'} name={'viewport'} />
+			<meta content={'en'} httpEquiv={'Content-Language'} />
+			<meta content={'TudoDS Configs. for Fish, ZSH and others'} name={'description'} />
+			<meta content={'TudoDS Configs. for Fish, ZSH and others'} name={'og:description'} />
+			<meta content={'summary_large_image'} name={'twitter:card'} />
+			<meta content={'https://my-configs.vercel.app/og.png'} name={'twitter:image'} />
+			<meta content={'my-configs.vercel.app'} name={'twitter:site:domain'} />
+			<meta content={'https://my-configs.vercel.app'} name={'twitter:url'} />
+			<meta content={'TutoDS | My Configs'} name={'og:title'} />
+			<meta content={'https://my-configs.vercel.app/og.png'} name={'og:image'} />
+			<meta content={'TutoDS | My Configs'} name={'apple-mobile-web-app-title'} />
+		</>
+	),
 	feedback: {
 		content: undefined
 	},
 	useNextSeoProps() {
 		return {
-			titleTemplate: '%s – TutoDS Configs'
+			titleTemplate: '%s | TutoDS Configs'
 		};
 	}
 };
