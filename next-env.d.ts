6487473e484111bf432c388a93d7041fb1b10aee
Daniel Sousa <jdaniel.asousa@gmail.com>
2023-10-01 10:50:58 +0100

    chore: replace exa by eza

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,9 +1,5 @@
-// eslint-disable-next-line multiline-comment-style
 // / <reference types="next" />
-// eslint-disable-next-line multiline-comment-style
 // / <reference types="next/image-types/global" />
 
-/*
- * NOTE: This file should not be edited
- * see https://nextjs.org/docs/basic-features/typescript for more information.
- */
+// NOTE: This file should not be edited
+// See https://nextjs.org/docs/basic-features/typescript for more information.
